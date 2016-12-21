1e51b9af2b19d8388977fe98e20d333a3f36fddd
Rudy <rudziik@wp.pl>
2016-12-21 23:52:24 +0100

    fdsf

---
+++ AdventureWorks/AdventureWorks/DataAccess/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AdventureWorks.DataAccess.Models
+{
+  public class Person
+  {
+    public Guid BusinessEntityID { get; set; }
+
+    public string PersonType { get; set; }
+
+    public bool NameStyle { get; set; }
+
+    public string Title { get; set; }
+
+    public string FirstName { get; set; }
+
+    public char MiddleName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Suffix { get; set; }
+
+    public int EmailPromotion { get; set; }
+
+    public string AdditionalContactInfo { get; set; }
+
+    public string Demographics { get; set; }    
+  }
+}
---
+++ AdventureWorks/AdventureWorks/DataAccess/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace AdventureWorks.DataAccess
+{
+  public class Person
+  {
+    private readonly SqlConnection ConnectionString = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["AdventureWorks"].ConnectionString);
+
+    public DataSet GetAllPersons()
+    {
+      SqlDataAdapter daPerson = new SqlDataAdapter();
+      DataSet dsPerson = new DataSet();
+      DataTable dtPerson = new DataTable();
+
+      daPerson.SelectCommand = new SqlCommand("SELECT * FROM Person.Person", ConnectionString);
+      daPerson.Fill(dsPerson, "Person");
+      dtPerson = dsPerson.Tables["Person"];
+
+      return dsPerson;
+    }
+  }
+}
